c12c659837968a09e96794c3d10dba6a14a6c856
Alexander Bazo <alexanderbazo@googlemail.com>
2020-11-23 22:22:48 +0100

    Prepare starter code

--- src/FlowerGarden/FlowerGarden.java
+++ src/FlowerGarden/FlowerGarden.java
@@ -1,12 +1,7 @@
 package FlowerGarden;
 
 import de.ur.mi.oop.app.GraphicsApp;
-import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
-import de.ur.mi.oop.graphics.Circle;
-import de.ur.mi.oop.graphics.Line;
-import de.ur.mi.oop.graphics.Point;
-import de.ur.mi.oop.graphics.Rectangle;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
 
 /**
@@ -17,101 +12,18 @@ import de.ur.mi.oop.launcher.GraphicsAppLauncher;
  */
 
 public class FlowerGarden extends GraphicsApp {
-    /**
-     * Wichtige Werte für die Konfiguration der Zeichenfläche und der darzustellenden Objekte werden
-     * an zentraler Stelle durch Konstanten abgebildet. Möglichst viele Werte werden aus bereits vorhandenen
-     * Konstanten berechnet. Dadurch müssen bei notwendigen Änderungen (z.B. der Größe der Zeichenfläche) nur eine
-     * kleine Anzahl an Stellen des Codes verändert werden.
-     */
-    private static final int SCREEN_WIDTH = 500;
-    private static final int SCREEN_HEIGHT = 500;
-    private static final Color BACKGROUND_COLOR = Colors.BLUE;
-    private static final Color GROUND_COLOR = Colors.GREEN;
-    private static final Color FLOWER_COLOR = Colors.GREEN;
-    private static final Color BLOSSOM_COLOR = Colors.RED;
-    private static final Color INNER_BLOSSOM_COLOR = Colors.YELLOW;
-    private static final float DEFAULT_FLOWER_SIZE = 20;
-    private static final int DEFAULT_FLOWER_WEIGHT = 5;
-    private static final int FLOWER_RADIUS = 15;
-    private static final float FLOWER_GROW_SPEED = 0.25f;
-    private static final int NUMBER_OF_FLOWERS = 10;
-    private static final int FLOWER_MARGIN = SCREEN_WIDTH / (NUMBER_OF_FLOWERS + 1);
 
-    /**
-     * Variablen für den "Boden", die Positionen der Blumen sowie deren aktuelle Höhe.
-     */
-    private Rectangle ground;
-    private Point[] flowerPositions;
-    private float currentFlowerSize = DEFAULT_FLOWER_SIZE;
+    private static final int CANVAS_WIDTH = 1280;
+    private static final int CANVAS_HEIGHT = 720;
 
     @Override
     public void initialize() {
-        setCanvasSize(SCREEN_WIDTH, SCREEN_HEIGHT);
-        initGround();
-        initFlowerPositions();
-    }
-
-    /**
-     * Initalisiert den Boden (die Wiese) auf der die Blumen wachsen. Der Boden wird als Rechteck am unteren Bildschirm-
-     * rand dargestellt und durch ein Objekt der Rectangle-Klasse dargestellt.
-     */
-    private void initGround() {
-        int xPositionGround = 0;
-        int yPositionGround = (int) (SCREEN_HEIGHT * 0.75);
-        ground = new Rectangle(xPositionGround, yPositionGround, SCREEN_WIDTH, SCREEN_HEIGHT - yPositionGround, GROUND_COLOR);
-    }
-
-    /**
-     * Initalisiert die Position bzw. den Ursprung der einzelnen Blumen in Form von Positon-Objekten, die eine x- und eine
-     * y-Koordinante speichern können. Die Positionen werden in einem Array (flowerPositions) gespeichert. Die Erzeugung
-     * und Speicherung erfolgt mit Hilfe eines Arrays. Dabei wird die y-Position mit jeder Iteration um einen bestimmten
-     * Wert erhöht um einen gleichmäßigen Abstand zwischen den Pflanzen zu erreichen.
-     */
-    private void initFlowerPositions() {
-        flowerPositions = new Point[NUMBER_OF_FLOWERS];
-        int xPosition = FLOWER_MARGIN;
-        int yPosition = (int) (SCREEN_HEIGHT * 0.75);
-        for (int i = 0; i < flowerPositions.length; i++) {
-            flowerPositions[i] = new Point(xPosition, yPosition);
-            xPosition += FLOWER_MARGIN;
-        }
+        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 
     @Override
     public void draw() {
-        drawBackground(BACKGROUND_COLOR);
-        drawScene();
-        /**
-         * Nach jedem Zeichenvorgang wird die Größe der Blumen für den nächsten Aufruf der draw-Methode erhöht: Dadurch
-         * wird das "Wachsen" der Blumen realisiert.
-         */
-        currentFlowerSize += FLOWER_GROW_SPEED;
-    }
-
-    /**
-     * In dieser ausgelagerten Methode werden zuerst alle Blumen über eine Schleife gezeichnet. Anschließend wird der
-     * Boden eingezeichnet. Die Reihenfolge der Zeichenbefehle bestimmt die z-Position der Objekte auf der Zeichen-
-     * fläche. Später gezeichnete Objekte überlagern bei Überschneidung solche Elemente, die vorher gezeichnet wurden.
-     */
-    private void drawScene() {
-        for (int i = 0; i < flowerPositions.length; i++) {
-            // Die aus dem Array ausgelesene Position wird an eine Methode übergeben, die auf Basis dieser Information
-            // eine einzelne Blume zeichnet.
-            drawFlowerAt(flowerPositions[i]);
-        }
-        ground.draw();
-    }
-
-    private void drawFlowerAt(Point position) {
-        // Zeichnet den Stängel der Blume als Linie von deren Position bis zur aktuellen Wuchshöhe
-        Line line = new Line(position.getXPos(), position.getYPos(), position.getXPos(), position.getYPos() - currentFlowerSize, FLOWER_COLOR, DEFAULT_FLOWER_WEIGHT);
-        line.draw();
-        // Zeichnet die äußere Blüte am Ende des gezeichneten Stängels
-        Circle blossom = new Circle(position.getXPos(), position.getYPos() - currentFlowerSize, FLOWER_RADIUS, BLOSSOM_COLOR);
-        blossom.draw();
-        // Zeichnet die innere, kleinere Blüte über die so eben gezeichnete äußere Blüte
-        Circle innerBlossom = new Circle(position.getXPos(), position.getYPos() - currentFlowerSize, FLOWER_RADIUS / 2, INNER_BLOSSOM_COLOR);
-        innerBlossom.draw();
+        drawBackground(Colors.WHITE);
     }
 
     public static void main(String[] args) {
--- src/MovingCar/MovingCar.java
+++ src/MovingCar/MovingCar.java
@@ -1,12 +1,8 @@
 package MovingCar;
 
 import de.ur.mi.oop.app.GraphicsApp;
-import de.ur.mi.oop.colors.Color;
 import de.ur.mi.oop.colors.Colors;
-import de.ur.mi.oop.graphics.Rectangle;
 import de.ur.mi.oop.launcher.GraphicsAppLauncher;
-
-import java.util.Random;
 
 /**
  * Diese GraphicsApp-Anwendung stellt ein Auto aus der Top-Down-Perspektive dar, das sich über den Bildschirm bewegt
@@ -15,185 +11,17 @@ import java.util.Random;
 
 public class MovingCar extends GraphicsApp {
 
-    /**
-     * Konstanten mit wichtigen, unveränderbaren Werten für die Darstellung des Autos und der übrigen Szene.
-     */
-    private static final int WIDTH = 500;
-    private static final int HEIGHT = WIDTH;
-    private static final Color BACKGROUND_COLOR = Colors.WHITE;
-    private static final Color CAR_BODY_COLOR = Colors.BLACK;
-    private static final Color CAR_WINDSHIELD_COLOR = Colors.WHITE;
-    private static final int CAR_WINDSHIELD_BORDER = 2;
-    private static final int CAR_WIDTH = 20;
-    private static final int CAR_LENGTH = 50;
-    private static final int CAR_SPEED = 1;
-    /**
-     * Numerische Werte für die Repräsentation der möglichen Bewegungsrichtungen des Autos. Durch die Beschreibung
-     * der Richtung als Zahl sind imm Programm einfache  Richtungsprüfungen und - wechsel möglich.
-     */
-    private static final int DIRECTION_NORTH = 0;
-    private static final int DIRECTION_EAST = 1;
-    private static final int DIRECTION_SOUTH = 2;
-    private static final int DIRECTION_WEST = 3;
-    /**
-     * Wahrscheinlichkeit, mit der in jedem Frame ein Wechsel der Bewegungsrichtung statt findet.
-     */
-    private static final double TURN_PROBABILITY = 0.05;
-    /**
-     * Aktuelle Position und Bewegungsrichtung des Autos.
-     */
-    private int xPosition;
-    private int yPosition;
-    private int currentDirection;
-    /**
-     * GraphicsObjects zum Zeichnen der Bestandteile des Autos.
-     */
-    private Rectangle car;
-    private Rectangle windshield;
-    /**
-     * Zufallsgenerator für die Berechung des Richtungsänderungen.
-     */
-    private Random rand = new Random();
+    private static final int CANVAS_WIDTH = 1280;
+    private static final int CANVAS_HEIGHT = 720;
 
     @Override
     public void initialize() {
-        /**
-         * Hier werden Größe der Zeichenfläche sowie Position und Bewegungsrichtung des Autos initial festgelegt.
-         */
-        setCanvasSize(WIDTH, HEIGHT);
-        xPosition = WIDTH / 2;
-        yPosition = HEIGHT / 2;
-        currentDirection = DIRECTION_NORTH;
-        /**
-         * Hier werden die graphischen Elemente (Rechtecke) zur Darstellung des Autos initialisiert.
-         */
-        car = new Rectangle(xPosition, yPosition, CAR_WIDTH, CAR_LENGTH, CAR_BODY_COLOR);
-        int windShieldSize = CAR_WIDTH - 2 * CAR_WINDSHIELD_BORDER;
-        windshield = new Rectangle(0, 0, windShieldSize, windShieldSize, CAR_WINDSHIELD_COLOR);
+        setCanvasSize(CANVAS_WIDTH, CANVAS_HEIGHT);
     }
 
     @Override
     public void draw() {
-        /**
-         * Bei jedem Aufruf der draw-Methode wird:
-         *  - der Hintergrund neu gezeichnet
-         *  - die Bewegungsrichtung, Position und Ausrichtung des Autos angepasst
-         *  - das Auto gezeichnet
-         */
-        drawBackground(BACKGROUND_COLOR);
-        updateCar();
-        drawCar();
-    }
-
-    private void updateCar() {
-        /**
-         * Prüft anhand einer Zufallszahl, ob eine Bewegungsänderung vorgenommen werden soll und passt ggf. den Wert der
-         * Instanzvariable "currentDirection" an.
-         */
-        tryToUpdateDirection();
-        /**
-         * Aktualisiert die Position des Autos auf Basis der aktuellen Bewegungsrichtung.
-         */
-        updatePosition();
-    }
-
-    private void tryToUpdateDirection() {
-        /**
-         * rand.nextInt(100) gibt eine uufällige Ganzzahl zwischen 0 (inklusive) und 100 (exklusive), also zwischen 0
-         * und 99, zurück. Die Zahl wird durch den double-Wert 100.0 geteilt.
-         */
-        double randomSeed = rand.nextInt(100) / 100.0;
-        /**
-         * Die angepasste Zufallszahl wird mit der in TURN_PROBABILITY gespeicherten Wahrscheinlichkeit verglichen. Liegt
-         * der Wert darunter, findet eine Bewegungsänderung statt.
-         */
-        if (randomSeed < TURN_PROBABILITY) {
-            /**
-             * Die Bewegungsrichtungen werden - in der Reihenfolge Nord, Ost, Süd, West - durch aufsteigende Ganzzahlen
-             * repräsentiert (0 = Norden, 1 = Osten, 2 = Süden, 3 = Westen). Bewegungsänderungen erfolgen immer im
-             * Uhrzeigersinn. Dies ist einfach durch die Inkrementierung des aktuellen Werts bzw. der aktuellen
-             * Richtung (gespeichert in currentDirection) möglich. Nach jeder Änderung wird geprüft, ob der aktuelle
-             * Wert außerhalb des Wertebereichs 0 bis 3 liegt. Dies ist der Fall, wenn die letzte Richtung "West" bzw.
-             * DIRECTION_WEST war. In diesem Fall wird die aktuelle Richtung auf "Norden" bzw. DIRECTION_NORTH gesetzt.
-             */
-            currentDirection++;
-            if (currentDirection > DIRECTION_WEST) {
-                currentDirection = DIRECTION_NORTH;
-            }
-        }
-    }
-
-    private void updatePosition() {
-        /**
-         * In Abhängigkeit der aktuellen Bewedungsrichtung werden x- bzw. y-Position des Autos angepasst. Die
-         * Geschwindkeit in Pixel per Frame wird der Konstanten CAR_SPEED entnommen. Da der Ursprung des Koordinaten-
-         * systems in der oberen, linken Ecke liegt, werden Bewegungen nach OST und SÜD durch positive und Bewegungen
-         * nach WEST und Nord durch negative Größenänderungen dargestellt.
-         */
-        if (currentDirection == DIRECTION_NORTH) {
-            yPosition -= CAR_SPEED;
-        }
-        if (currentDirection == DIRECTION_EAST) {
-            xPosition += CAR_SPEED;
-        }
-        if (currentDirection == DIRECTION_SOUTH) {
-            yPosition += CAR_SPEED;
-        }
-        if (currentDirection == DIRECTION_WEST) {
-            xPosition -= CAR_SPEED;
-        }
-        /**
-         * Die Position des Rechtecks, das das Auto repräsentiert, wird mit Hilfe der neuen Positionswerte aktualisiert.
-         */
-        car.setPosition(xPosition, yPosition);
-    }
-
-    private void drawCar() {
-        drawCarBody();
-        drawWindShield();
-    }
-
-    private void drawCarBody() {
-        /**
-         * In Abhängigkeit der aktuellen Bewegungsrichtung werden Länge und Breite des Autos ausgewählt.
-         */
-        int carWidth = CAR_WIDTH;
-        int carHeight = CAR_LENGTH;
-        if (currentDirection == DIRECTION_EAST || currentDirection == DIRECTION_WEST) {
-            carWidth = CAR_LENGTH;
-            carHeight = CAR_WIDTH;
-        }
-        /**
-         * Die berechneten Werte werden für die Anpassung des Rechtecks verwendet, welches anschließend gezeichnet wird.
-         */
-        car.setWidth(carWidth);
-        car.setHeight(carHeight);
-        car.draw();
-    }
-
-    private void drawWindShield() {
-        /**
-         * Die Windschutzscheibe soll, mit leichtem Versatz (CAR_WINDSHIELD_BORDER) an der aktuellen Position des Autos
-         * eingezeichnet werden.
-         */
-        int windshieldXPosition = xPosition + CAR_WINDSHIELD_BORDER;
-        int windshieldYPosition = yPosition + CAR_WINDSHIELD_BORDER;
-        /**
-         * Fährt das Auto aktuell nach Osten oder Süden, muss die Position der Windschutzscheibe angepasst werden. Die
-         * Position wird auf Basis der aktuellen x- bzw. y-Koordinate des Autos, dessen Länge, dem Abstand der Wind-
-         * schutzscheibe zum Auto und der Größe der Scheibe berechnet.
-         */
-        if (currentDirection == DIRECTION_EAST) {
-            windshieldXPosition = (int) (xPosition + CAR_LENGTH - CAR_WINDSHIELD_BORDER - windshield.getWidth());
-        }
-        if (currentDirection == DIRECTION_SOUTH) {
-            windshieldYPosition = (int) (yPosition + CAR_LENGTH - CAR_WINDSHIELD_BORDER - windshield.getWidth());
-        }
-        /**
-         * Die berechneten Werte werden für die Anpassung des Rechtecks verwendet, welches anschließend gezeichnet wird.
-         */
-        windshield.setPosition(windshieldXPosition, windshieldYPosition);
-        windshield.draw();
+        drawBackground(Colors.WHITE);
     }
 
     public static void main(String[] args) {
